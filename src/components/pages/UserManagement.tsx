fe6956f01e7a33f1b8cf71a1d8dc7bfa0cb24d4a
takumibv <everybody.say.hey.yo@gmail.com>
2021-12-01 23:09:02 +0900

    UserCard実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,38 @@
-import { memo, VFC } from "react";
+import { Image } from "@chakra-ui/image";
+import { Box, Center, Stack, Text, Wrap, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserResponse } from "../../types/api/userResponse";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <div>UserManagement</div>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner size="lg" />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user: UserResponse) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+
+    </>
+  );
 })
